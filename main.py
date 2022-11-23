f621348bf545a51515d02dedf15a0946289e4ea3
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-23 21:17:50 +0200

    finished 3.12 and refactoring

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ r = -0.9
 Y_threshold = My - sigma_y  # 24.5
 X_arr = range(13, 17)
 
-sigma_coefs = list(set(np.array([*map(lambda x: [-x, x], [0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30])]).flatten()))
+coefs_bases = [0, 1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
+sigma_coefs = list(set(np.array([*map(lambda x: [-x, x], coefs_bases)]).flatten()))
 sigma_coefs.sort()
+
+do_show_graphics = True
 
 
 def print_task(no):
@@ -89,13 +92,13 @@ def get_W_x_y(x, y):
 
 def get_W_y_given_x(x, y):
     a = 1 / (sigma_y * math.sqrt(2 * math.pi * (1 - r ** 2)))
-    b = (1 / (2 * sigma_y ** 2 * (1 - r ** 2))) * (y - My * ((sigma_y * (x - Mx)) / sigma_x)) ** 2
+    b = (1 / (2 * sigma_y ** 2 * (1 - r ** 2))) * (y - My - ((sigma_y * (x - Mx)) / sigma_x)) ** 2
     return a * math.exp(-b)
 
 
 def get_W_x_given_y(x, y):
     a = 1 / (sigma_x * math.sqrt(2 * math.pi * (1 - r ** 2)))
-    b = (1 / (2 * sigma_x ** 2 * (1 - r ** 2))) * (x - Mx * ((sigma_x * (y - My)) / sigma_y)) ** 2
+    b = (1 / (2 * sigma_x ** 2 * (1 - r ** 2))) * (x - Mx - ((sigma_x * (y - My)) / sigma_y)) ** 2
     return a * math.exp(-b)
 
 
@@ -186,13 +189,24 @@ def get_M_y_if_is_decision_K2():
 def get_W_y_given_decision_K1(y):
     x_lower, x_upper = get_X_K1_limits()
     fun = lambda x: get_W_x_given_y(x, y)
-    return integrate.quad(fun, x_lower, x_upper)[0]
+    # return integrate.quad(fun, x_lower, x_upper)[0]
+    return (1 / get_P_dec_K1()) * integrate.quad(fun, x_lower, x_upper)[0]
 
 
 def get_W_y_given_decision_K2(y):
     x_lower, x_upper = get_X_K2_limits()
     fun = lambda x: get_W_x_given_y(x, y)
-    return integrate.quad(fun, x_lower, x_upper)[0]
+    # return integrate.quad(fun, x_lower, x_upper)[0]
+    return (1 / get_P_dec_K2()) * integrate.quad(fun, x_lower, x_upper)[0]
+
+
+def get_L_x(x):
+    y_lower_K1, y_upper_K1 = get_Y_K1_limits()
+    y_lower_K2, y_upper_K2 = get_Y_K2_limits()
+    fun = lambda y: get_W_y_given_x(x, y)
+    a = (1 / get_P_K1()) * integrate.quad(fun, y_lower_K1, y_upper_K1)[0]
+    b = (1 / get_P_K2()) * integrate.quad(fun, y_lower_K2, y_upper_K2)[0]
+    return a / b
 
 
 def main():
@@ -224,11 +238,12 @@ def main():
     print(label)
     print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
-    plt.plot(x_arr, w_x_arr)
-    plt.xlabel("x")
-    plt.ylabel("W(x)")
-    plt.title(label)
-    plt.show()
+    if do_show_graphics:
+        plt.plot(x_arr, w_x_arr)
+        plt.xlabel("x")
+        plt.ylabel("W(x)")
+        plt.title(label)
+        plt.show()
 
     # W(x/K1), W(x/K2)
     label = "Значення умовних густин розподілу ознаки за умови, що примірник належить до класу К1 або К2"
@@ -254,13 +269,14 @@ def main():
     print(label)
     print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
-    plt.plot(x_K1_arr, w_x_K1_arr, label="W(x/K1)")
-    plt.plot(x_K2_arr, w_x_K2_arr, label="W(x/K2)")
-    plt.xlabel("x")
-    plt.ylabel("W(x/K)")
-    plt.legend()
-    plt.title(label)
-    plt.show()
+    if do_show_graphics:
+        plt.plot(x_K1_arr, w_x_K1_arr, label="W(x/K1)")
+        plt.plot(x_K2_arr, w_x_K2_arr, label="W(x/K2)")
+        plt.xlabel("x")
+        plt.ylabel("W(x/K)")
+        plt.legend()
+        plt.title(label)
+        plt.show()
 
     # X_threshold
     X_threshold = get_X_threshold()
@@ -297,11 +313,12 @@ def main():
     print(label)
     print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
-    plt.plot(y_arr, w_y_arr)
-    plt.xlabel("y")
-    plt.ylabel("W(y)")
-    plt.title(label)
-    plt.show()
+    if do_show_graphics:
+        plt.plot(y_arr, w_y_arr)
+        plt.xlabel("y")
+        plt.ylabel("W(y)")
+        plt.title(label)
+        plt.show()
 
     # W(y|ріш К1), W(y|ріш К2)
     label = "Умовні розподіли прогнозованого параметра за умови, якщо приймається рішення про віднесення до класу К1 " \
@@ -328,16 +345,21 @@ def main():
     print(label)
     print(tabulate(table_data, headers=col_names, tablefmt="fancy_grid"))
 
-    plt.plot(y_K1_arr, w_y_decision_K1_arr, label="W(y|ріш K1)")
-    plt.plot(y_K2_arr, w_y_decision_K2_arr, label="W(y|ріш K2)")
-    plt.xlabel("y")
-    plt.ylabel("W(y| ріш K)")
-    plt.legend()
-    plt.title(label)
-    plt.show()
+    if do_show_graphics:
+        plt.plot(y_K1_arr, w_y_decision_K1_arr, label="W(y|ріш K1)")
+        plt.plot(y_K2_arr, w_y_decision_K2_arr, label="W(y|ріш K2)")
+        plt.xlabel("y")
+        plt.ylabel("W(y| ріш K)")
+        plt.legend()
+        plt.title(label)
+        plt.show()
 
     # Відношення правдоподібності для 4-х значень ознаки x
-
+    print()
+    print_task(3.12)
+    print("Відношення правдоподібності L(x) для чотирьох значень ознаки X:")
+    for i in range(len(X_arr)):
+        print("L(x{}) = {}".format(i + 1, get_L_x(X_arr[i])))
 
 
 if __name__ == '__main__':
